Fix run # in loading datasets, and improve power/peak calculation.

plot_channel.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 from utils import *
 
 CHANNEL = 79
@@ -14,16 +15,19 @@ fig, ax = plt.subplots(figsize=(12, 4))
 
 psds = all_psds[CHANNEL]
 psds_db = convert_psds(psds, dB=True)
+interp_psds, interp_freqs = insert_band_boundaries(psds, freqs)
+interp_psds_db = convert_psds(interp_psds, dB=True)
+
 ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel(get_unit_label(dB=True))
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
-ax.plot(freqs, psds_db, lw=1, color='black')
+ax.plot(interp_freqs, interp_psds_db, lw=1, color='black')
 
 for name in BANDS.keys():
     band = BANDS[name]
-    idx = np.logical_and(freqs >= band.min_freq, freqs <= band.max_freq)
-    plt.fill_between(freqs, psds_db, y2=psds_db.min(), where=idx, color=band.color)
+    idx = np.logical_and(interp_freqs >= band.min_freq, interp_freqs <= band.max_freq)
+    plt.fill_between(interp_freqs, interp_psds_db, y2=interp_psds_db.min(), where=idx, color=band.color)
 
 ax.tick_params(length=3)
 
@@ -40,7 +44,7 @@ tab.auto_set_column_width([0, 1, 2])
 tab.auto_set_font_size(False)
 tab.set_fontsize(9)
 
-peaks = get_peaks(all_psds)
+peaks, _ = scipy.signal.find_peaks(psds_db, prominence=1)
 for peak in peaks:
     ax.vlines(freqs[peak], psds_db.min(), psds_db[peak], color='darkblue', lw=0.5, linestyles='dashed')
     ax.text(freqs[peak], psds_db[peak], '%0.3f' % psds_db[peak], fontsize=9, ha='center', va='bottom')

utils/analysis.py
@@ -1,4 +1,4 @@
-__all__ = ['convert_psds', 'get_peaks', 'get_power', 'get_unit_label']
+__all__ = ['convert_psds', 'get_power', 'get_unit_label', 'insert_band_boundaries']
 
 import numpy as np
 import scipy
@@ -21,16 +21,32 @@ def convert_psds(psds, dB=False, estimate='power', unit='µV'):
         np.multiply(psds, 10, out=psds)
     return psds
 
-def get_peaks(all_psds):
+def get_peaks0(all_psds):
     counts = np.bincount(np.argmax(all_psds, axis=1))
     peaks, _ = scipy.signal.find_peaks(np.concatenate(([0], counts, [0])))
     return peaks - 1
 
 def get_power(psds, freqs, band):
+    idx = np.logical_and(freqs > band.min_freq, freqs < band.max_freq)
+    y = psds[idx]
+    x = freqs[idx]
+
+    left = np.interp(band.min_freq, freqs, psds)
+    y = np.insert(y, 0, left)
+    x = np.insert(x, 0, band.min_freq)
+
+    right = np.interp(band.max_freq, freqs, psds)
+    y = np.append(y, right)
+    x = np.append(x, band.max_freq)
+
+    y = convert_psds(y)
+    return scipy.integrate.simps(y, x)
+
+def get_power0(psds, freqs, band):
     freq_res = (freqs[-1] - freqs[0]) / len(freqs)
     idx = np.logical_and(freqs >= band.min_freq, freqs <= band.max_freq)
-    psds = convert_psds(psds)
-    return scipy.integrate.simps(psds[idx], dx=freq_res)
+    y = convert_psds(psds[idx])
+    return scipy.integrate.simps(y, dx=freq_res)
 
 def get_unit_label(dB=False, estimate='power', unit='µV'):
     if estimate == 'auto':
@@ -45,3 +61,15 @@ def get_unit_label(dB=False, estimate='power', unit='µV'):
     if dB:
         label += r'$\ \mathrm{(dB)}$'
     return label
+
+def insert_band_boundaries(psds, freqs):
+    for band in BANDS.values():
+        for freq in [band.min_freq, band.max_freq]:
+            idx = freqs.searchsorted(freq, 'right') - 1
+            if idx < 0:
+                psds = np.insert(psds, 0, np.interp(freq, freqs, psds))
+                freqs = np.insert(freqs, 0, freq)
+            elif freqs[idx] < freq:
+                psds = np.insert(psds, idx + 1, np.interp(freq, freqs, psds))
+                freqs = np.insert(freqs, idx + 1, freq)
+    return (psds, freqs)